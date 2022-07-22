3deda6ec1a4525d68d3036186dceb456c46a6ba1
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-22 20:58:00 +0300

    commit tree 9da9b1b4dcc2c6342baf55ed9795e277fdd76fcd

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-// take in the url
 function useQuery(url) {
   const [isLoaded, setIsLoaded] = useState(false);
-  // rename `posts` to a more generic `data`
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([null]);
 
   useEffect(() => {
     setIsLoaded(false);
@@ -15,11 +13,9 @@ function useQuery(url) {
         setIsLoaded(true);
       });
   }, [url]);
-  // the url is now a dependency
-  // we want to use the side effect whenever the url changes
 
   // return an *object* with the data and isLoaded state
-  return { data, isLoaded };
+  return {data, isLoaded};
 }
 
 export default useQuery;
